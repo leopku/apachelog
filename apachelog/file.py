ea45cc5cf6ff5fdace7ced45b537d96213767dc8
W. Trevor King <wking@drexel.edu>
2012-02-18 13:40:47 -0500

    Add apachelog.file.open, which makes it easy to ignore compressession in processors.

---
+++ apachelog/file.py
@@ -0,0 +1,30 @@
+import gzip as _gzip
+import os.path as _os_path
+
+
+"""Openers by file extention.
+
+Values should be callables such that::
+
+  for line in opener(filename, mode):
+      ...
+
+will work.
+"""
+OPENERS = {
+    '.gz': _gzip.open,
+    }
+
+
+def open(filename, openers=None):
+    """Utility method that decompresses files based on their extension.
+
+    Uses ``OPENERS`` to determine the appropriate opener for the
+    file's extension.  If the extension is not listed in ``OPENERS``,
+    fall back to the ``open`` builtin.
+    """
+    if openers is None:
+        openers = OPENERS
+    extension = _os_path.splitext(filename)[-1]
+    opener = openers.get(extension, open)
+    return opener(filename, 'r')
